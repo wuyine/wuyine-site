12db4d75bc02800322b26ec98ab507b884918024
wuyine <lijinlong@nears.cn>
2020-05-13 19:25:30 +0800

    rebuilding site 2020年 5月13日 星期三 19时25分30秒 CST

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t cleanwhite # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo --theme='https://wuyine.github.io/' --baseUrl # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
